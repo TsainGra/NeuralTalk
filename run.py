5fd5b6bab0fd379a540dbc93013d1beddfec8c25
PranaySinghal <singhal.pranay@gmail.com>
2017-09-26 16:30:37 +0000

    Add files via upload

--- run.py
+++ run.py
@@ -1,48 +1,43 @@
 import glob
 from PIL import Image
 import numpy as np
-import os
 # import matplotlib.pyplot as plt
 import pickle
 from tqdm import tqdm
 import pandas as pd
 from keras.preprocessing import sequence
 from keras.models import Sequential
-from keras.layers import LSTM, Embedding, TimeDistributed, Dense, RepeatVector, Merge, Activation, Flatten
+from keras.layers import Add, LSTM, Embedding, TimeDistributed, Dense, RepeatVector, Activation, Flatten, Merge
+from keras.layers import concatenate
 from keras.optimizers import Adam, RMSprop
 from keras.layers.wrappers import Bidirectional
 from keras.applications.inception_v3 import InceptionV3
 from keras.preprocessing import image
+from keras.models import Model
 
-#extract each captions
 token = 'Flickr8k_text/Flickr8k.token.txt'
 captions = open(token, 'r').read().strip().split('\n')
 d = {}
 for i, row in enumerate(captions):
     row = row.split('\t')
     row[0] = row[0][:len(row[0])-2]
-    ## collect all captions belonging to same picture under same key
-    if row[0] in d:                 
+    if row[0] in d:
         d[row[0]].append(row[1])
     else:
         d[row[0]] = [row[1]]
-
 images = 'Flickr8k_Dataset/'
-
 # Contains all the images
-
-img = [os.path.basename(x) for x in glob.glob('Flickr8k_Dataset/*.jpg')]
+img = glob.glob(images+'*.jpg')
 train_images_file = 'Flickr8k_text/Flickr_8k.trainImages.txt'
 train_images = set(open(train_images_file, 'r').read().strip().split('\n'))
 def split_data(l):
     temp = []
     for i in img:
-        if i in l:                  # if the image exists in the caption file, then append it in the list
-            temp.append(i)
+        temp.append(i)
     return temp
 # Getting the training images from all the images
 train_img = split_data(train_images)
-
+len(train_img)
 val_images_file = 'Flickr8k_text/Flickr_8k.devImages.txt'
 val_images = set(open(val_images_file, 'r').read().strip().split('\n'))
 # Getting the validation images from all the images
@@ -52,10 +47,8 @@ test_images_file = 'Flickr8k_text/Flickr_8k.testImages.txt'
 test_images = set(open(test_images_file, 'r').read().strip().split('\n'))
 # Getting the testing images from all the images
 test_img = split_data(test_images)
-len(test_img)
-Image.open(images + train_img[0])
 
-
+Image.open(train_img[0])
 def preprocess_input(x):
     x /= 255.
     x -= 0.5
@@ -64,14 +57,15 @@ def preprocess_input(x):
 def preprocess(image_path):
     img = image.load_img(image_path, target_size=(299, 299))
     x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)  # convert multi-dim to single-dim
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
     return x
 from keras.models import Model
 model = InceptionV3(weights='imagenet')
 
 new_input = model.input
-hidden_layer = model.layers[-2].output  # second last layer outputs encoded img, last layer is used for classification
+hidden_layer = model.layers[-2].output
 
 model_new = Model(new_input, hidden_layer)
 
@@ -80,29 +74,28 @@ def encode(image):
     temp_enc = model_new.predict(image)
     temp_enc = np.reshape(temp_enc, temp_enc.shape[1])
     return temp_enc
-
 encoding_train = {}
-for img in tqdm(train_img):         #tqdm is used to show progress
-    encoding_train[img[len(images):]] = encode(img)  # encode each image
-
-with open("encoded_images_inceptionV3.p", "wb") as encoded_pickle:
-    pickle.dump(encoding_train, encoded_pickle)                         #pickle dump each image to serialize
+# for img in tqdm(train_img):
+#     encoding_train[img[len(images):]] = encode(img)
+#
+# with open("encoded_images_inceptionV3.p", "wb") as encoded_pickle:
+#     pickle.dump(encoding_train, encoded_pickle)
 
 encoding_train = pickle.load(open('encoded_images_inceptionV3.p', 'rb'))
 
 encoding_test = {}
-for img in tqdm(test_img):
-    encoding_test[img[len(images):]] = encode(img)
-
-with open("encoded_images_test_inceptionV3.p", "wb") as encoded_pickle:
-    pickle.dump(encoding_test, encoded_pickle)
+# for img in tqdm(test_img):
+#     encoding_test[img[len(images):]] = encode(img)
+#
+# with open("encoded_images_test_inceptionV3.p", "wb") as encoded_pickle:
+#     pickle.dump(encoding_test, encoded_pickle)
 
 encoding_test = pickle.load(open('encoded_images_test_inceptionV3.p', 'rb'))
 
 train_d = {}
 for i in train_img:
     if i[len(images):] in d:
-        train_d[i] = d[i[len(images):]]     # extract only the image name and strip the directory name
+        train_d[i] = d[i[len(images):]]
 
 val_d = {}
 for i in val_img:
@@ -117,12 +110,11 @@ for i in test_img:
 caps = []
 for key, val in train_d.items():
     for i in val:
-        caps.append('<start> ' + i + ' <end>')    # <start> and <end> keyword is used to mark start and end of captions,
-                                                  # It is useful later when we want to predict word by word using images everytime
+        caps.append('<start> ' + i + ' <end>')
 
 words = [i.split() for i in caps]
 
-unique = []                                     # This is to find number of unique words, helpful in embedding layer defn.
+unique = []
 for i in words:
     unique.extend(i)
 unique = list(set(unique))
@@ -134,12 +126,10 @@ unique = list(set(unique))
 
 #len(unique)
 
-# defining dictionary to get corresponding index for word and vice versa
 word2idx = {val:index for index, val in enumerate(unique)}
 
 idx2word = {index:val for index, val in enumerate(unique)}
 
-# calculating max length of captions, useful in embedding layer defn.
 max_len = 0
 for c in caps:
     c = c.split()
@@ -168,7 +158,7 @@ len(c)
 imgs = [i for i in df['image_id']]
 
 a = c[-1]
-# sample output of each word and unique index of captions
+
 for i in a.split():
     print (i, "=>", word2idx[i])
 
@@ -216,25 +206,44 @@ def data_generator(batch_size = 32):
                         next_words = np.asarray(next_words)
                         images = np.asarray(images)
                         partial_caps = sequence.pad_sequences(partial_caps, maxlen=max_len, padding='post')
+                        # x=[[images, partial_caps], next_words]
+                        # print(x.shape[0], x.shape[1])
                         yield [[images, partial_caps], next_words]
                         partial_caps = []
                         next_words = []
                         images = []
                         count = 0
 
+# print(z)
+# print(len(z), " ", len(z)," ", len(z))
+# print(len(z[0]), " ",len(z[1]))
+# print(len(z[0][0]), " ", len(z[0][1], " ", len(z[1][0])))
+# print(len(z[0][0][0])," ", len(z[0][1][0]), " ", len(z[1][0][0]))
+
+
+
+
+
+
+# print(len(z[0][1][0][0]))
+# print(len(z[0][1][0][0][0]))
+# print(len(z[0][1][0][0][0][0]))
+
 embedding_size = 300
 
-#repeatvector is used as same img will be used to predict captoins word by word
 image_model = Sequential([
        Dense(embedding_size, input_shape=(2048,), activation='relu'),
       RepeatVector(max_len)
       ])
-# Timedistributed applies Dense layer to each time-step
+
 caption_model = Sequential([
           Embedding(vocab_size, embedding_size, input_length=max_len),
           LSTM(256, return_sequences=True),
            TimeDistributed(Dense(300))
                     ])
+
+#conc = concatenate([image_model, caption_model], axis=1)
+# print(type([image_model, caption_model]))
 
 final_model = Sequential([
                         Merge([image_model, caption_model], mode='concat', concat_axis=1),
@@ -243,12 +252,16 @@ final_model = Sequential([
                         Activation('softmax')
                     ])
 
+#final_model = Model(inputs=[image_model, caption_model], outputs=final_mode)
 final_model.compile(loss='categorical_crossentropy', optimizer=RMSprop(), metrics=['accuracy'])
 
 final_model.summary()
-final_model.fit_generator(data_generator(batch_size=128), steps_per_epoch=50000,
+final_model.fit_generator(data_generator(batch_size=256), steps_per_epoch=25,
                                               nb_epoch=1,
-                                              verbose=0)
+                                              )
+#final_model.save_weights("model.h5")
+#final_model.load_weights("model.h5")
+
 
 def predict_captions(image):
     start_word = ["<start>"]
@@ -292,7 +305,7 @@ def beam_search_predictions(image, beam_index=3):
         start_word = temp
         # Sorting according to the probabilities
         start_word = sorted(start_word, reverse=False, key=lambda l: l[1])
-        # Getting the top words
+        # Getting the top word
         start_word = start_word[-beam_index:]
 
     start_word = start_word[-1][0]
